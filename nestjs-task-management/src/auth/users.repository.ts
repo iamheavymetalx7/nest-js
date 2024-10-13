922a78af9b7f1184f9f8fc66104b7b2ada20d923
iamheavymetalx7 <nitishkumarmay30@gmail.com>
2024-10-13 18:22:53 +0530

    authentication bearer-jwt-tokens

---
+++ nestjs-task-management/src/auth/users.repository.ts
@@ -0,0 +1,43 @@
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
+import {
+  ConflictException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import * as bcrypt from 'bcrypt';
+@Injectable()
+export class UsersRepository {
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
+  ) {}
+
+  async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    const { username, password } = authCredentialsDto;
+    // hash
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const user = this.usersRepository.create({
+      username,
+      password: hashedPassword,
+    });
+    try {
+      await this.usersRepository.save(user);
+    } catch (error) {
+      //   console.log(error.code);
+      if (error.code === '23505') {
+        throw new ConflictException('username already exists');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
+  }
+  // Add the missing findOne method
+  async findOne(username: string): Promise<User | undefined> {
+    return this.usersRepository.findOne({ where: { username } });
+  }
+}
